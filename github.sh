d748804974732927bafa8a742fa80de1f3b8769f
tiamo405 <nam05052002@gmail.com>
2023-06-01 15:14:26 +0700

    update data test

--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 git remote add fast-reid-cxview https://github.com/tiamo405/fast-reid-cxview.git
 git add .
-git commit -m 'update'
+git commit -m 'update data test'
 git push fast-reid-cxview master
